eedcb2a092182fc3594c265c3a7d5ecb7d9401bb
123123 <123123123@qq.com>
2021-02-21 16:42:28 +0800

    gxxdd

--- code/index.dart
+++ code/index.dart
@@ -1,67 +1,64 @@
 import 'package:flutter/material.dart';
-
 class Page0 extends StatelessWidget {
 @override
 Widget build(BuildContext context) {
 return Container(
 child: Stack(
 children: <Widget>[Positioned(
-child: Block_453311(),
-width: 348.00,
-height: 58.50,
-top: 0.00,
-left: 9.00
+child: Block_(),
+width: 696.00.w,
+height: 117.00.w,
+top: 0.00.w,
+left: 18.00.w
 ),
-Shape_1()]
+Shape_()]
 ),
-height: 480.00
+width: 750.00.w,
+height: 960.00.w
 );
 }
-}class Block_453311 extends StatelessWidget {
+}class Block* extends StatelessWidget {
 @override
 Widget build(BuildContext context) {
 return Container(
-width: 348.00,
-height: 58.50,
+width: 696.00.w,
+height: 117.00.w,
 child: Row(
 crossAxisAlignment: CrossAxisAlignment.center,
 mainAxisAlignment: MainAxisAlignment.spaceBetween,
 children: <Widget>[Container(
-height: 58.50,
+height: 117.00.w,
 child: Row(
 crossAxisAlignment: CrossAxisAlignment.center,
 mainAxisAlignment: MainAxisAlignment.start,
 children: <Widget>[Image(
 image: new NetworkImage('https://img.alicdn.com/tfs/TB1oXyBx4D1gK0jSZFyXXciOVXa-111-197.png'),
 fit: BoxFit.fill,
-width: 37.00,
-height: 58.50
+width: 74.00.w,
+height: 117.00.w
 ),
-Container(
-child: Text('妈妈',
+Text('妈妈',
 textAlign: TextAlign.left,
 style: TextStyle(color: Color.fromARGB(255, 79, 105, 159),
-fontSize: 17.00)
-),
-margin: const EdgeInsets.fromLTRB(6.00,0,0,0)
+fontSize: 34.00)
 )]
 )
 ),
 Image(
 image: new NetworkImage('https://img.alicdn.com/tfs/TB1mBeAx.Y1gK0jSZFCXXcwqXXa-45-11.png'),
 fit: BoxFit.fill,
-width: 15.00,
-height: 3.50
+width: 30.00.w,
+height: 7.00.w
 )]
 )
 );
 }
 }
-class Block_253434 extends StatelessWidget {
+class Block* extends StatelessWidget {
 @override
 Widget build(BuildContext context) {
 return Container(
-height: 228.00,
+height: 456.00.w,
 child: Column(
 crossAxisAlignment: CrossAxisAlignment.start,
 mainAxisAlignment: MainAxisAlignment.start,
@@ -71,192 +68,136 @@ mainAxisAlignment: MainAxisAlignment.start,
 children: <Widget>[Image(
 image: new NetworkImage('https://img.alicdn.com/tfs/TB1ieWzx5_1gK0jSZFqXXcpaXXa-222-223.png'),
 fit: BoxFit.fill,
-width: 111.00,
-height: 111.50
+width: 222.00.w,
+height: 223.00.w
 ),
-Container(
-child: Image(
+Image(
 image: new NetworkImage('https://img.alicdn.com/tfs/TB172uzx7L0gK0jSZFxXXXWHVXa-222-224.png'),
 fit: BoxFit.fill,
-width: 111.00,
-height: 112.00
+width: 222.00.w,
+height: 224.00.w
 ),
-width: 111.00,
-height: 112.00,
-margin: const EdgeInsets.fromLTRB(5.00,0,0,0)
-),
-Container(
-child: Image(
+Image(
 image: new NetworkImage('https://img.alicdn.com/tfs/TB1adixx7P2gK0jSZPxXXacQpXa-222-223.png'),
 fit: BoxFit.fill,
-width: 111.00,
-height: 111.50
-),
-width: 111.00,
-height: 111.50,
-margin: const EdgeInsets.fromLTRB(5.00,0,0,0)
+width: 222.00.w,
+height: 223.00.w
 )]
 ),
-Container(
-margin: const EdgeInsets.fromLTRB(0,4.50,0,0),
-child: Row(
+Row(
 crossAxisAlignment: CrossAxisAlignment.start,
 mainAxisAlignment: MainAxisAlignment.start,
 children: <Widget>[Image(
 image: new NetworkImage('https://img.alicdn.com/tfs/TB154msxWL7gK0jSZFBXXXZZpXa-222-223.png'),
 fit: BoxFit.fill,
-width: 111.00,
-height: 111.50
+width: 222.00.w,
+height: 223.00.w
 ),
-Container(
-child: Image(
+Image(
 image: new NetworkImage('https://img.alicdn.com/tfs/TB1ZMmAx4D1gK0jSZFKXXcJrVXa-222-223.png'),
 fit: BoxFit.fill,
-width: 111.00,
-height: 111.50
+width: 222.00.w,
+height: 223.00.w
 ),
-width: 111.00,
-height: 111.50,
-margin: const EdgeInsets.fromLTRB(5.00,0,0,0)
-),
-Container(
-child: Image(
+Image(
 image: new NetworkImage('https://img.alicdn.com/tfs/TB1Bu1zx7L0gK0jSZFxXXXWHVXa-222-223.png'),
 fit: BoxFit.fill,
-width: 111.00,
-height: 111.50
-),
-width: 111.00,
-height: 111.50,
-margin: const EdgeInsets.fromLTRB(5.00,0,0,0)
+width: 222.00.w,
+height: 223.00.w
 )]
-)
 )]
 )
 );
 }
 }
-class Block_798308 extends StatelessWidget {
+class Block* extends StatelessWidget {
 @override
 Widget build(BuildContext context) {
 return Container(
-width: 343.00,
-height: 36.50,
+width: 686.00.w,
+height: 73.00.w,
 child: Row(
 crossAxisAlignment: CrossAxisAlignment.center,
 mainAxisAlignment: MainAxisAlignment.start,
-children: <Widget>[Container(
-child: Image(
+children: <Widget>[Image(
 image: new NetworkImage('https://img.alicdn.com/tfs/TB1f7Kwx.T1gK0jSZFrXXcNCXXa-59-56.png'),
 fit: BoxFit.fill,
-width: 19.50,
-height: 18.50
+width: 39.00.w,
+height: 37.00.w
 ),
-width: 19.50,
-height: 18.50,
-margin: const EdgeInsets.fromLTRB(7.50,0,0,0)
-),
-Container(
-child: Text('赞',
+Text('赞',
 textAlign: TextAlign.left,
 style: TextStyle(color: Color.fromARGB(255, 43, 43, 53),
-fontSize: 14.00)
+fontSize: 28.00)
 ),
-margin: const EdgeInsets.fromLTRB(5.00,1.00,0,0)
-),
-Container(
-child: Image(
+Image(
 image: new NetworkImage('https://img.alicdn.com/tfs/TB10sywxVP7gK0jSZFjXXc5aXXa-54-54.png'),
 fit: BoxFit.fill,
-width: 18.00,
-height: 18.00
+width: 36.00.w,
+height: 36.00.w
 ),
-width: 18.00,
-height: 18.00,
-margin: const EdgeInsets.fromLTRB(35.00,1.00,0,0)
-),
-Container(
-child: Text('评论',
+Text('评论',
 textAlign: TextAlign.left,
 style: TextStyle(color: Color.fromARGB(255, 43, 43, 53),
-fontSize: 14.00)
+fontSize: 28.00)
 ),
-margin: const EdgeInsets.fromLTRB(7.00,0,0,0)
-),
-Container(
-child: Image(
+Image(
 image: new NetworkImage('https://img.alicdn.com/tfs/TB1XIiwx.H1gK0jSZSyXXXtlpXa-48-54.png'),
 fit: BoxFit.fill,
-width: 16.00,
-height: 18.00
+width: 32.00.w,
+height: 36.00.w
 ),
-width: 16.00,
-height: 18.00,
-margin: const EdgeInsets.fromLTRB(34.00,1.00,0,0)
-),
-Container(
-child: Text('分享',
+Text('分享',
 textAlign: TextAlign.left,
 style: TextStyle(color: Color.fromARGB(255, 43, 43, 53),
-fontSize: 14.00)
-),
-margin: const EdgeInsets.fromLTRB(8.00,0,0,0)
+fontSize: 28.00)
 )]
 )
 );
 }
 }
-class Block_855681 extends StatelessWidget {
+class Block* extends StatelessWidget {
 @override
 Widget build(BuildContext context) {
 return Container(
-height: 50.00,
-margin: const EdgeInsets.fromLTRB(16.00,7.50,0,0),
+height: 100.00.w,
 child: Column(
 crossAxisAlignment: CrossAxisAlignment.start,
 mainAxisAlignment: MainAxisAlignment.start,
 children: <Widget>[Text('爸爸点了赞',
 textAlign: TextAlign.left,
 style: TextStyle(color: Color.fromARGB(255, 79, 105, 159),
-fontSize: 15.00)
+fontSize: 30.00)
 ),
-Container(
-margin: const EdgeInsets.fromLTRB(0,8.00,0,0),
-child: Row(
+Row(
 crossAxisAlignment: CrossAxisAlignment.start,
 mainAxisAlignment: MainAxisAlignment.start,
 children: <Widget>[Text('奶奶',
 textAlign: TextAlign.left,
 style: TextStyle(color: Color.fromARGB(255, 79, 105, 159),
-fontSize: 15.00)
+fontSize: 30.00)
 ),
-Container(
-child: Text(':',
+Text(':',
 textAlign: TextAlign.left,
 style: TextStyle(color: Color.fromARGB(255, 79, 105, 159),
-fontSize: 15.00)
+fontSize: 30.00)
 ),
-margin: const EdgeInsets.fromLTRB(4.00,0,0,0)
-),
-Container(
-child: Text('一下子就是大姑娘了',
+Text('一下子就是大姑娘了',
 textAlign: TextAlign.left,
 style: TextStyle(color: Color.fromARGB(255, 43, 43, 53),
-fontSize: 15.00)
-),
-margin: const EdgeInsets.fromLTRB(5.00,0,0,0)
+fontSize: 30.00)
 )]
-)
 )]
 )
 );
 }
 }
-class Shape_1 extends StatelessWidget {
+class Shape* extends StatelessWidget {
 @override
 Widget build(BuildContext context) {
 return Container(
-height: 480.00,
+width: 750.00.w,
+height: 960.00.w,
 child: Column(
 crossAxisAlignment: CrossAxisAlignment.start,
 mainAxisAlignment: MainAxisAlignment.start,
@@ -265,24 +206,21 @@ children: <Widget>[Container(
 child: Text('今年元旦节，全家人一起去了迪士尼乐园，天气也好到爆炸，大宝开心的跳起来了',
 textAlign: TextAlign.left,
 style: TextStyle(color: Color.fromARGB(255, 43, 43, 53),
-fontSize: 17.00)
+fontSize: 34.00)
 ),
-width: 374.50,
-height: 48.00,
-margin: const EdgeInsets.fromLTRB(0,40.50,0,0)
+width: 749.00.w,
+height: 96.00.w
 )]
 ),
 Container(
-child: Block_253434(),
-width: 374.50,
-margin: const EdgeInsets.fromLTRB(0,9.00,0,0)
+child: Block*(),
+width: 749.00.w
 ),
 Container(
-child: Block_798308(),
-width: 374.50,
-margin: const EdgeInsets.fromLTRB(0,15.00,0,0)
+child: Block*(),
+width: 749.00.w
 ),
-Block_855681()]
+Block*()]
 ),
 decoration: BoxDecoration(color: Color.fromARGB(255, 255, 255, 255))
 );
